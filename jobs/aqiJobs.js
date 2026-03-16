d6fe959a42a9ab39eda60c294018990299586f9c
Sawan Kumar Jha <sawanjha2004@gmail.com>
2026-03-16 12:52:28 +0530

    made some changes

--- jobs/aqiJobs.js
+++ jobs/aqiJobs.js
@@ -1,9 +1,19 @@
-import { fetchExternal, writeJSON, AQI_FILE } from "../utils/fileHelpers.js";
+import { writeJSON, AQI_FILE } from "../utils/fileHelpers.js";
 
 export async function updateAQI() {
   try {
-    const data = await fetchExternal(process.env.AQI_API);
+    const res = await fetch(process.env.AQI_API, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.AQI_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+
     writeJSON(AQI_FILE, data);
+
     console.log("🌫️ AQI updated");
   } catch (e) {
     console.log("❌ AQI failed:", e.message);
